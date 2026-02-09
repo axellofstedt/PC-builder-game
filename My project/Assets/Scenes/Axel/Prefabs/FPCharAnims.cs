609f24658ff4e173c2112cee18cc59020a888cf7
axellofstedt <axel.lofstedt@gmail.com>
2026-02-09 17:34:30 +0100

    made the first person character
    it's located in Scenes/Axel/Prefabs as MainCharacter

---
+++ My project/Assets/Scenes/Axel/Prefabs/FPCharAnims.cs
@@ -0,0 +1,78 @@
+using System;
+using UnityEngine;
+
+public class FPCharAnims : MonoBehaviour
+{
+    private Animator animator;
+    private bool isRunning = false;
+    private bool Wdown = false;
+    private bool Adown = false;
+    private bool Sdown = false;
+    private bool Ddown = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void interactAnimation()
+    {
+        animator.SetTrigger("interact");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Wdown = true;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Adown = true;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Sdown = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Ddown = true;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Wdown = false;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Adown = false;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            Sdown = false;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            Ddown = false;
+        }
+        if ((Wdown || Adown || Sdown || Ddown) && !isRunning)
+        {
+            isRunning = true;
+            animator.SetTrigger("run");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isRunning = false;
+            animator.SetTrigger("jump");
+        }
+        if(!Wdown && !Adown && !Sdown && !Ddown && isRunning)
+        {
+            isRunning = false;
+            animator.SetTrigger("idle");
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            interactAnimation();
+        }
+    }
+}
