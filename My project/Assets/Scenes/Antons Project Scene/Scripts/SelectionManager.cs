60ec03f920164472a82756ecd0e01791c175fae9
Nummer1InfFan <123357877+Nummer1InfFan@users.noreply.github.com>
2026-02-11 11:07:50 +0100

    Text on screen

--- My project/Assets/Scenes/Antons Project Scene/Scripts/SelectionManager.cs
+++ My project/Assets/Scenes/Antons Project Scene/Scripts/SelectionManager.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using TMPro;
 
 public class SelectionManager : MonoBehaviour
 {
    public static SelectionManager Instance;
    private Selectable selectedObject;
+   public TextMeshProUGUI heldItemText;
 
     void Awake()
     {
@@ -41,26 +43,41 @@ public class SelectionManager : MonoBehaviour
                 Debug.Log("CPU placed on motherboard" + selectedObject.GetPartName());
                 PlaceSelectedObject(hit.point);
             }
-            else if (hit.collider.CompareTag("CPU_Cooling") && selectedObject.getPartType().ToString() == "CPU_Cooling")
+            else if (hit.collider.CompareTag("CPU_Cooling") && selectedObject.GetPartType().ToString() == "CPU_Cooling")
             {
                 Debug.Log("CPU cooler placed on CPU" + selectedObject.GetPartName());
                 PlaceSelectedObject(hit.point);
             }
-            else if (hit.collider.CompareTag("Drive") && selectedObject.getPartType().ToString() == "Drive")
+            else if (hit.collider.CompareTag("Drive") && selectedObject.GetPartType().ToString() == "Drive")
             {
                 Debug.Log("Drive placed on motherboard" + selectedObject.GetPartName());
                 PlaceSelectedObject(hit.point);
             }
-        }
-        if (selectedObject != null && (hit.collider.CompareTag("Motherboard") && selectedObject.GetPartType().ToString() =="PSU"))
-        {
-            Debug.Log("Placed on motherboard" + selectedObject.GetPartName());
-            PlaceSelectedObject(hit.point);
-        }
-        else if (selectedObject != null && hit.collider.CompareTag("PlacementSurface"))
-        {
-            //PlaceSelectedObject(hit.point);
-            Debug.Log("Cantplacehere");
+            else if (hit.collider.CompareTag("Fan") && selectedObject.GetPartType().ToString() == "Fan")
+            {
+                Debug.Log("Fan placed in chassi" + selectedObject.GetPartName());
+                PlaceSelectedObject(hit.point);
+            }
+            else if (hit.collider.CompareTag("GPU") && selectedObject.GetPartType().ToString() == "GPU")
+            {
+                Debug.Log("Graphics card placed on motherboard" + selectedObject.GetPartName());
+                PlaceSelectedObject(hit.point);
+            }
+            else if (hit.collider.CompareTag("Motherboard") && selectedObject.GetPartType().ToString() == "Motherboard")
+            {
+                Debug.Log("Motherbard placed in chassi" + selectedObject.GetPartName());
+                PlaceSelectedObject(hit.point);
+            }
+            else if (hit.collider.CompareTag("PSU") && selectedObject.GetPartType().ToString() == "PSU")
+            {
+                Debug.Log("Power supply placed in chassi" + selectedObject.GetPartName());
+                PlaceSelectedObject(hit.point);
+            }
+            else if (hit.collider.CompareTag("RAM") && selectedObject.GetPartType().ToString() == "RAM")
+            {
+                Debug.Log("RAM placed on motherboard" + selectedObject.GetPartName());
+                PlaceSelectedObject(hit.point);
+            }
         }
     }
 
@@ -68,11 +85,29 @@ public class SelectionManager : MonoBehaviour
     {
         selectedObject = obj;
         Debug.Log("Selected"+obj.GetPartName());
+        UpdateHeldItemUI();
     }
 
     void PlaceSelectedObject(Vector3 position)
     {
         selectedObject.transform.position = position;
         selectedObject = null;
+        UpdateHeldItemUI();
+    }
+
+    void UpdateHeldItemUI()
+    {
+        if (selectedObject == null)
+        {
+            heldItemText.text = "";
+            return;
+        }
+
+        string partName = selectedObject.GetPartName();
+        //string instruction = GetInstructionForPart(selectedObject.GetPartType());
+
+        heldItemText.text =
+            $"Du håller i: {partName}\n" +
+            $"Place correctly in chassi";
     }
 }
